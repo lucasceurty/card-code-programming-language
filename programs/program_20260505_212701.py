7663936f8a5cd416ca9ca4333dc15614fbae36b4
Lucas Ceurty <lucasceurty@Lucass-Laptop.local>
2026-05-05 22:16:41 -0700

    Website upload

---
+++ programs/program_20260505_212701.py
@@ -0,0 +1,3 @@
+for _ctr in range(1, ((10 + 10)) + 1):
+    if ((_ctr % 2) == 0):
+        print(_ctr)
---
+++ programs/program_20260505_214141.py
@@ -0,0 +1,3 @@
+for _ctr in range(1, ((10 + 5)) + 1):
+    if (not ((_ctr % 2) == 0)):
+        print(_ctr)
